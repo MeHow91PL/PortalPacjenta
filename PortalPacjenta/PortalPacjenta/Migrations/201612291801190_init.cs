e4f92b9e8ad11693e716765f36172c561cb00b3e
Filip <pc@PC-KOMPUTER>
2017-01-06 19:00:49 +0100

    commit

--- PortalPacjenta/PortalPacjenta/Migrations/201612291801190_init.cs
+++ PortalPacjenta/PortalPacjenta/Migrations/201612291801190_init.cs
@@ -3,7 +3,7 @@ namespace PortalPacjenta.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Init : DbMigration
+    public partial class init : DbMigration
     {
         public override void Up()
         {
@@ -180,10 +180,37 @@ namespace PortalPacjenta.Migrations
                 .ForeignKey("dbo.Pracownicy", t => t.PracownikID)
                 .Index(t => t.PracownikID);
             
+            CreateTable(
+                "dbo.Wizyty",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DataWizyty = c.DateTime(nullable: false),
+                        godzOd = c.String(),
+                        godzDo = c.String(),
+                        DataModyfikacji = c.DateTime(nullable: false),
+                        PracownikID = c.Int(nullable: false),
+                        PacjentID = c.Int(nullable: false),
+                        RezerwacjaId = c.Int(),
+                        Rozpoznanie = c.String(),
+                        Badanie = c.String(),
+                        Wywiad = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Pacjenci", t => t.PacjentID, cascadeDelete: true)
+                .ForeignKey("dbo.Pracownicy", t => t.PracownikID, cascadeDelete: true)
+                .ForeignKey("dbo.Rezerwacje", t => t.RezerwacjaId)
+                .Index(t => t.PracownikID)
+                .Index(t => t.PacjentID)
+                .Index(t => t.RezerwacjaId);
+            
         }
         
         public override void Down()
         {
+            DropForeignKey("dbo.Wizyty", "RezerwacjaId", "dbo.Rezerwacje");
+            DropForeignKey("dbo.Wizyty", "PracownikID", "dbo.Pracownicy");
+            DropForeignKey("dbo.Wizyty", "PacjentID", "dbo.Pacjenci");
             DropForeignKey("dbo.Uzytkowicy", "PracownikID", "dbo.Pracownicy");
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
@@ -195,6 +222,9 @@ namespace PortalPacjenta.Migrations
             DropForeignKey("dbo.Pracownicy", "Adres_Id", "dbo.Adresy");
             DropForeignKey("dbo.Pacjenci", "AdresTymczasowy_Id", "dbo.Adresy");
             DropForeignKey("dbo.Pacjenci", "Adres_Id", "dbo.Adresy");
+            DropIndex("dbo.Wizyty", new[] { "RezerwacjaId" });
+            DropIndex("dbo.Wizyty", new[] { "PacjentID" });
+            DropIndex("dbo.Wizyty", new[] { "PracownikID" });
             DropIndex("dbo.Uzytkowicy", new[] { "PracownikID" });
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
@@ -208,6 +238,7 @@ namespace PortalPacjenta.Migrations
             DropIndex("dbo.Pracownicy", new[] { "Adres_Id" });
             DropIndex("dbo.Pacjenci", new[] { "AdresTymczasowy_Id" });
             DropIndex("dbo.Pacjenci", new[] { "Adres_Id" });
+            DropTable("dbo.Wizyty");
             DropTable("dbo.Uzytkowicy");
             DropTable("dbo.AspNetUserLogins");
             DropTable("dbo.AspNetUserClaims");
